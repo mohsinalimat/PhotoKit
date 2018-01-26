bd5ad5d0e280bd8ce8fcfd4ec4f718e2ae031684
Elias Abel <admin@meniny.cn>
2018-01-27 00:34:08 +0800

    ignore pods

--- Pods/Target Support Files/SuperAlertController/SuperAlertController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SuperAlertController : NSObject
-@end
-@implementation PodsDummy_SuperAlertController
-@end
--- Pods/Target Support Files/SuperAlertControllerExtensions/SuperAlertControllerExtensions-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SuperAlertControllerExtensions : NSObject
-@end
-@implementation PodsDummy_SuperAlertControllerExtensions
-@end
